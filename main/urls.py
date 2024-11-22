bdb2052b8d70783e4f9f193854a0415d35da58a4
Jych1a0 <wo.jych1a0@gmail.com>
2024-11-22 05:44:19 -0800

    initialization

---
+++ main/urls.py
@@ -0,0 +1,29 @@
+"""data_mgt URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import os
+from dotenv import load_dotenv
+from django.contrib import admin
+from django.urls import path, include
+
+load_dotenv()
+data_mgt_version = os.environ.get("DATA_MGT_VERSION")
+
+urlpatterns = [
+    path(f'api/{data_mgt_version}/', include('main.apps.inference_layer_data_mgt.api.urls')),
+    path(f'api/{data_mgt_version}/', include('main.apps.model_file_mgt.api.urls')),
+    path(f"api/{data_mgt_version}/", include('main.apps.central_layer_data_mgt.api.urls')),
+    path('admin/', admin.site.urls),
+]
