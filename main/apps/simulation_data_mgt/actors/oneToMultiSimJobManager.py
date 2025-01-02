ac7249043d831c233cdfb1ea66d499ebd1fb41a1
jych1a0 <wo.jych1a0@gmail.com>
2025-01-02 08:55:15 +0800

    feature: add oneToMultiSimJobManager

--- main/apps/simulation_data_mgt/actors/oneToMultiSimJobManager.py
+++ main/apps/simulation_data_mgt/actors/oneToMultiSimJobManager.py
@@ -71,11 +71,11 @@ def run_oneToMulti_simulation_async(oneToMulti_uid):
         container_name = f"oneToMultiSimulation_{oneToMulti_uid}"
 
         if isinstance(obj.oneToMulti_parameter, dict):
-            simulation_command = f"/root/mercury/shell/simulation_script.sh '{json.dumps(obj.oneToMulti_parameter)}'"
+            simulation_command = f"/root/mercury/shell/simulation_oneToMulti_script.sh '{json.dumps(obj.oneToMulti_parameter)}'"
         else:
             try:
                 param_dict = json.loads(obj.oneToMulti_parameter)
-                simulation_command = f"/root/mercury/shell/simulation_script.sh '{json.dumps(param_dict)}'"
+                simulation_command = f"/root/mercury/shell/simulation_oneToMulti_script.sh '{json.dumps(param_dict)}'"
             except json.JSONDecodeError:
                 simulation_command = obj.oneToMulti_parameter
 
@@ -87,7 +87,7 @@ def run_oneToMulti_simulation_async(oneToMulti_uid):
             '--rm',
             f'--name={container_name}',
             '-v', f'{os.path.abspath(simulation_result_dir)}:/root/mercury/build/service/output',
-            'oneToMultisimulationimage_86400',
+            'chiao2',
             'bash', '-c', simulation_command
         ]
 
