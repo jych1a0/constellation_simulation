62fb7cd6563c06be5fa0929d1351cee76c2de63f
jych1a0 <wo.jych1a0@gmail.com>
2025-03-07 11:27:28 +0800

    feature: update file

--- main/apps/simulation_data_mgt/actors/saveErRoutingSimJobManager.py
+++ main/apps/simulation_data_mgt/actors/saveErRoutingSimJobManager.py
@@ -107,7 +107,7 @@ def run_saveErRouting_simulation_async(saveErRouting_uid):
         else:
             raise Exception("Unable to get container process ID")
 
-        timeout = 60 * 60 * 24
+        timeout = 60 * 60 * 24 * 3
         start_time = time.time()
 
         while True:
